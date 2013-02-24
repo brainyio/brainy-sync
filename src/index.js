36bab1c93f0902499226d490400d7b0728df3384
catshirt <caden@catshirt.net>
2013-02-24 15:00:22 -0500

    adding db adapters

--- src/index.js
+++ src/index.js
@@ -1,58 +1,10 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-define([
-  'mongodb',
-  'underscore',
-  './collection',
-  './model'
-], function(mongodb, _, cSync, mSync) {
+define(function() {
 
-  var Server = mongodb.Server,
-    Db = mongodb.Db;
-
-  return function(config) {
-    var host = config.host,
-      port = config.port,
-      name = config.name,
-      connection = new Server(host, port);
-    
-    var client = new Db(name, connection, {
-      w: 1
-    });
-
-    client.open(function(err, client) { });
-    
-    return function(method, resource, options) {
-      var is_collection = resource.models,
-        name = resource.urlRoot || resource.url,
-        sync = is_collection? cSync: mSync,
-        attrs = options.attr || _.extend(resource.toJSON(options), options.data || {});
-
-      var success = options.success;
-      options.success = function(resp) {
-        if (success) success(resource, resp, options);
-        resource.trigger('sync', resource, resp, options);
-      };
-
-      var error = options.error;
-      options.error = function(status) {
-        if (error) error(resource, status, options);
-        resource.trigger('error', resource, status, options);
-      };
-    
-      var params = { 
-      //  capped: true,
-      //  size: 100000
-      };
-
-      (function(name, attrs, options) {
-        client.createCollection(name, function(err, collection) {
-          sync[method](collection, attrs, options);
-        });
-      })(name, attrs, options);
-      
-      resource.trigger('request', resource, null, options);
-    };
-  }
+  return function(adapter_name, config) {
+    var adapter = require('./' + adapter_name + '/index');
+    return adapter(config);
+  };
 
 });
